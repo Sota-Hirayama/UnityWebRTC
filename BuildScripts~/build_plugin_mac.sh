66f4319742857a8d8943eb2c8bfc2a280d5930e0
Sota-Hirayama <sota.hirayama@mixi.co.jp>
2026-02-02 16:56:27 +0900

    Revert "Revert "M117をMac向けにビルドできるように""

--- BuildScripts~/build_plugin_mac.sh
+++ BuildScripts~/build_plugin_mac.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eu
 
+# TODO : このスクリプトはUnityWebRTCのルートディレクトリで実行することを前提としている
+
+export COMMAND_DIR=$(cd $(dirname $0); pwd)
+export ZIP_PATH=$(pwd)/BuildScripts~/artifacts/webrtc-mac.zip
 export LIBWEBRTC_DOWNLOAD_URL=https://github.com/Unity-Technologies/com.unity.webrtc/releases/download/M116-20250805/webrtc-mac.zip
 export SOLUTION_DIR=$(pwd)/Plugin~
 export DYLIB_FILE=$(pwd)/Runtime/Plugins/macOS/libwebrtc.dylib
@@ -16,13 +20,20 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 brew install cmake
 
 # Download LibWebRTC 
-curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
+##curl -L $LIBWEBRTC_DOWNLOAD_URL > webrtc.zip
+echo "cp WebRTC.zip"
+rm -rf ./webrtc.zip
+cp ${ZIP_PATH} ./webrtc.zip
+
+echo "unzip webRtc.zip"
 unzip -d $SOLUTION_DIR/webrtc webrtc.zip 
+
+echo "remove old dylib"
 
 # Remove old dylib file
 rm -rf "$DYLIB_FILE"
 
 # Build UnityRenderStreaming Plugin
 cd "$SOLUTION_DIR"
-cmake --preset=macos -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE
-cmake --build --preset=release-macos --target=WebRTCPlugin
+cmake --preset=macos -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DBUILD_TESTING=OFF
+cmake --build --preset=release-macos --target=WebRTCPlugin --verbose
